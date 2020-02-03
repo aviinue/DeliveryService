f5072a05f1ca028fafb8f2152e9a6b06d98fff55
Kathryn Nastasi <avii0nat@gmail.com>
2020-02-03 17:02:53 -0600

    Player movement, player drops plants, colliders

---
+++ DeliveryService/Assets/Scripts/DropController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropController : MonoBehaviour
+{
+    public KeyCode drop;
+    public GameObject myObject;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (Input.GetKeyDown(drop))
+        {
+            Instantiate(myObject, transform.position + (transform.forward * 2), transform.rotation);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Target")
+        {
+            Destroy(myObject);
+            //this.GetComponent<scoreScript>().score += 1;
+        }
+    }
+}
---
+++ DeliveryService/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    public int speed = 5;                //Floating point variable to store the player's movement speed
+    private Rigidbody2D rb2d;          //Store a reference to Rigidbody2D component required to use 2D Physics
+    public KeyCode moveUp;
+    public KeyCode moveDown;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(moveUp))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed);
+        }
+        else if (Input.GetKey(moveDown))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, speed * -1);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+    }
+}
